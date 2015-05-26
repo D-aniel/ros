c4d9e2a9ffc848a80be6a1df869e5e7fb89d767a
Daniel <dcai637@cse.unsw.edu.au>
2015-05-27 01:21:44 +1000

    final

--- connector/listener.py
+++ connector/listener.py
@@ -8,12 +8,19 @@ import subprocess as subp
 from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist
 from movebase import movement
 import re
+from request import Request
 
 class listener():
 	string = ""
 	move = movement()
+	req = Request()
+	
+	
 	def callback(self,data):
-
+		if self.req.status == Request.MOTION:
+			rospy.loginfo("I'm busy at the moment, going to %s",self.req.currLoc)
+			return
+			
 		self.string = data.data
 		#rospy.loginfo("callbackheard: %s", data.data)
 		self.p.stdin.write(self.string+'.')
@@ -24,13 +31,67 @@ class listener():
 		result = self.p.stdout.readline()
 		#process framescript bridge
 		
-		match = re.match(r'\? : (.*) (.*)',result)
+		match = re.match(r'\? : ([a-zA-Z]+) (.*)',result)
 		if match:
+			if match.group(1) == "checkOrder":
+				rospy.loginfo("So you are after %s", match.group(2))
+				self.req.currOrder = match.group(2)
+			elif match.group(1) == "cancelOrder":
+				rospy.loginfo("I must of misheard, what did you want to do again?")
+				self.req.currOrder = ""
+			elif match.group(1) == "confirmOrder":
+				if self.req.currOrder:
+					rospy.loginfo("Ok I'll bring %s with me", self.req.currOrder)
+					self.req.setOrder()
+				elif not self.req.currOrder:
+					rospy.loginfo("You haven't told me what to bring")
+				self.currOrder = ""
+			elif match.group(1) == "checkLocation":
+				rospy.loginfo("You're sitting at %s?",match.group(2))
+				self.req.currLoc = match.group(2)
+			elif match.group(1) == "confirmLocation":
+				rospy.loginfo("Ok, I'll note down that you're sitting there")
+				self.req.location = self.req.currLoc
+			elif match.group(1) == "cancelLocation":
+				rospy.loginfo("I must have heard the wrong location, what did want to do again?")
+				self.req.currLoc = ""
+			elif match.group(1) == "setFavourite":
+				rospy.loginfo("Your new favourite is %s", match.group(2))
+				self.req.currFav = match.group(2)
+			elif match.group(1) == "queryFav":
+				if self.req.currFav:
+					rospy.loginfo("What did you want me to do with %s", self.req.currFav)
+				else:
+					rospy.loginfo("I don't know your favourite drink")	
+			elif match.group(1) == "getFavourite":
+				if self.req.currFav:
+					rospy.loginfo("Ok, I'll grab a %s", self.req.currFav)
+					self.req.currOrder = self.req.currFav
+					self.req.setOrder()
+				else:
+					rospy.loginfo("I don't know what your favourite is")
+					#self.p.stdin.write("setfavourite")
+					#result = self.p.stdout.readline()
+			elif match.group(1) == "failSafe":
+				rospy.loginfo("I dont understand %s",match.group(2))
+			elif match.group(1) == "finishOrder":
+				if self.req.isReady():
+					rospy.loginfo("Ok I'm heading off now")
+					rospy.loginfo("%s %s", self.req.location, ''.join(str(o) for o in self.req.order))
+					
+					msg = self.req.location + " " + ''.join(str(o) for o in self.req.order)
+					self.pub.publish(msg)
+				elif not self.req.location:
+					rospy.loginfo("Where do you want me to take them")
+				else:
+					rospy.loginfo("You haven't ordered anything")
 			rospy.loginfo("matched: %s(%s)",match.group(1),match.group(2))
+			
 		else:
 			rospy.loginfo("unmatched: %s",result)
 		
-		#self.pub.publish(result)
+		
+		
 		#rospy.loginfo("init finished %s: %s", self.string,result)
 			
 	def init(self):
@@ -42,12 +103,12 @@ class listener():
 		self.string = ""
 		rospy.loginfo("Listening...")
 		cmd = "java -cp mica/framescript.jar unsw.cse.framescript.FrameScript mica/scripts/grammar.frs mica/scripts/bot.frs".split()
+		#cmd = "java -cp mica/framescript.jar unsw.cse.framescript.FrameScript mica/scripts/ordering.frs".split()
 		self.p = subp.Popen(cmd, stdin=subp.PIPE,stdout=subp.PIPE)
 		#rospy.loginfo("init: %s",p.stdout.readline()) #ignore Framescript init text
-		self.currLocation = ""
-		self.currOrder = ""
 		self.p.stdout.readline()
 		self.p.stdin.write('Hello.')
+		#self.req = request()
 		rospy.loginfo("Init hello %s", self.p.stdout.readline())
 		#while not rospy.is_shutdown():
 			#rospy.loginfo("Listening..")
--- connector/movebase.py~
+++ connector/movebase.py~
@@ -8,22 +8,25 @@ from std_msgs.msg import String
 import subprocess as subp
 from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist
 import re
+from request import Request
 
 class movement():
-		
+	loc = ""
 	def movePoint(self,loc):
 		move_base = None
 		goal = MoveBaseGoal()
 		goal.target_pose.header.frame_id = 'map'
 		goal.target_pose.header.stamp = rospy.Time.now()
-		
-		dock = Point(10.2, -8.79, 0.408)
-		door = Point(11.1, -4.34, 0.408)
-		deska = Point(12.4,-10.5, 0.408)
-		deskb = Point(8.82,-7.9, 0.408)
-		
+				
 		#goal.target_pose.pose = Pose(Point(10.2, -8.79, 0.408), Quaternion(0.000, 0.000, 0.892, -1.500))
-		goal.target_pose.pose = Pose(Point(11.4, -4.34, 0.408), Quaternion(0.000, 0.000, 0.892, -1.500))
+		p = Point(10.1, -8.47, 0.408)
+		if loc == "desk a":
+			p = Point(12.4, -10.5, 0.408)
+		elif loc == "desk b":
+			p = Point(8.82, -7.9, 0.408)
+		elif loc == "door":
+			p = Point(11.1, -4.34, 0.408)
+		goal.target_pose.pose = Pose(p, Quaternion(0.000, 0.000, 0.892, -1.500))
 		#start moving
 		self.move_base.send_goal(goal)
 		rospy.loginfo("moveforward:Sending goal")
@@ -38,31 +41,39 @@ class movement():
 				#starting calli
 		rospy.loginfo("Finding goal...")
 	
+	def moveArm(self,items):
+		self.pub.publish(items)	
+		rospy.loginfo("Publishing items%s", items)
+	
+	def armfinish(self, data):
+		if data.data == "Free":
+			rospy.loginfo("Going to %s", self.loc)
+			rospy.loginfo("Bridge finished hearing:%s", data.data)
+			self.movePoint(self.loc)
+			Request.status = Request.PENDING
+					
+		
 	def bridge(self,data):
 		rospy.loginfo("bridge:==%s", data.data)
-		match = re.match(r': (.*) (.*)',data.data)
+		Request.status = Request.MOTION
+		match = re.match(r'(.*) ([01]+)',data.data)
 		#rospy.loginfo("matched: %s",match.group(2))
+		rospy.loginfo("Turtlebot Docking")
+		self.loc = match.group(1)
+		self.movePoint("dock")
+		#item = 	list(match.group(2))
+		rospy.loginfo("Picking up %s",match.group(2))
 		
-		if data.data == "moveForward":
-			rospy.loginfo("Calling moveForward")
-			#new thread
-			#self.moveforward()
-		elif match.group(1) == "pickup":
-			rospy.loginfo("Picking up %s",match.group(2))
-			self.pub.publish(match.group(2))
-		elif data.data == "saveFavourite(^2)":
-			rospy.loginfo("saveFavourite")
-		rospy.loginfo("Bridge finished hearing:%s", data.data)
-			
+		self.moveArm(match.group(2))
+
 	
 	def init(self):
 		self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
 		rospy.loginfo("wait for the action server to come up")
 		#self.move_base.wait_for_server(rospy.Duration(2))
 		rospy.Subscriber("mvbase", String, self.bridge) 
+		rospy.Subscriber("jacofinish", String, self.armfinish)
 		self.pub = rospy.Publisher("pickup", String, queue_size=10) 
-		self.favourite = ""
-		self.user = 0
 
 		
 
